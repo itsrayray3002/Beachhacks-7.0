a46dec6b094b80e5ac27171658ad336b7a52d6f5
Hunter <34562875+WheeledParasite@users.noreply.github.com>
2023-02-26 12:48:03 -0800

    FINALLY got SSL integration to work Co-authored-by: Mark Carson <MarkCarsonDev@users.noreply.github.com> Co-authored-by: natefedida <natexhussle@users.noreply.github.com> Co-authored-by: itsrayray3002 <itsrayray3002@users.noreply.github.com>

--- scripts/stackcollab_app.py
+++ scripts/stackcollab_app.py
@@ -1,18 +1,36 @@
-import os
+
 from flask import Flask, render_template, request, url_for, redirect
 from flaskext.mysql import MySQL
 from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify
 import mysql.connector
+import secrets
+import pathlib
+from OpenSSL import SSL
+
+
+from flask_dance.contrib.linkedin import make_linkedin_blueprint, linkedin # LINKEDIN
+
 #from flask_login import login_required, current_user, login_user
 #from flask_dance.consumer import OAuth2ConsumerBlueprint
 #rom flask_dance.consumer.storage.sqla import SQLAlchemyStorage
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
 # Configuring and Accessing the Database
 # mysql_obj = MySQL()
 db = SQLAlchemy()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:lWGTQm32UWinY8GqQzby@localhost/sql_test'
 db.init_app(app)
+
+certfile = pathlib.Path(__file__).parent / 'localhost.pem'
+keyfile = pathlib.Path(__file__).parent / 'localhost-key.pem'
+
+context = (certfile, keyfile)
+
+# context = SSL.Context(SSL.TLSv1_2_METHOD)
+# context.use_certificate(certfile)
+# context.use_privatekey(keyfile)
 
 class User(db.Model):
    user_id = db.Column(db.Integer, primary_key=True) #readded
@@ -37,6 +55,13 @@ class Swipes(db.Model):
 
    def __repr__(self):
       return '<Task %r>' % self.user_id
+   
+class match(db.Model):
+   user_id = db.Column(db.Integer, primary_key=True)
+   target = db.Column(db.Integer, unique=False)
+
+   def __repr__(self):
+      return '<Task %r>' % self.user_id
 
 # create a connector
 mysql_connector = mysql.connector.connect(host="localhost", 
@@ -54,16 +79,30 @@ mycursor.execute("SELECT * FROM User")
 for row in mycursor:
    print(row)
 
-#  linkedin_bp = OAuth2ConsumerBlueprint(
-#     # DO NOT PUSH TO PROD AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-#     client_id="860v4mkeeipykm",
-#     client_secret="dSuEAHyAxPXRqcu4",
-#     # DO NOT PUSH TO PROD AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-# )
+linkedin_bp = make_linkedin_blueprint( #linkedin
+    client_id="860v4mkeeipykm",
+    client_secret="dSuEAHyAxPXRqcu4",
+    scope="r_liteprofile r_emailaddress",
+    redirect_url="https://127.0.0.1:5000/swipe")
+
+app.register_blueprint(linkedin_bp, url_prefix="/login") #linkedin
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
       return render_template("login.html")
+
+@app.route("/login/linkedin") #linkedin
+def login():
+    if not linkedin.authorized:
+        return redirect(url_for("linkedin.login"))
+    resp = linkedin.get("/v2/emailAddress?q=members&projection=(elements*(handle~))")
+    if resp.ok:
+        email = resp.json()["elements"][0]["handle~"]["emailAddress"]
+        # login the user
+        return redirect(url_for("swipe"))
+    else:
+        return "Could not fetch email from LinkedIn."
+
 
 @app.route('/profile', methods=['POST', 'GET'])
 def profile():
@@ -136,12 +175,15 @@ def matches():
    
 @app.route('/swipe', methods=['POST', 'GET'])
 def swipe():
+   # Hardcoded test-user profile
+   profile = User.query.filter_by(user_id=80).first()
 
+   # Grab first user to test match with
    target = User.query.first()
-   matched = Swipes(profile.user_id)
+   matched = Swipes(profile.user_id, profile.email, None)
    if request.method == 'POST':
       user = User.query.first() # TODO: replace with current user
-
+      return render_template("swipe.html", profile=target)
    #    if user is None:
    #       print("User not found, creating! (sourced from swipe)")
    #       user = User(email=request.form['email'], fname=request.form['fname'], age=request.form['age'], bio=request.form['bio'], github=request.form['github'])
@@ -166,10 +208,34 @@ def swipe():
    #          printError(e)
    else:
       return render_template("swipe.html", profile=target)
+      approved = request.form['approved']
+      if approved:
+         matched.target = target.user_id
+         db.session.add(matched)
+         db.session.commit()
+      else:
+         # TODO: find the next USER
+         pass
+
+   def is_matched(cur_user): 
+      mycursor.execute("SELECT user_id")
+      for user in mycursor:
+         if (cur_user.target == user.user_id) and (user.target == cur_user.user_id):
+            matched = match(cur_user.user_id, target.user_id)
+            mycursor.execute("")
+            db.session.add(matched)
+            db.session.commit()
+
 
 @app.route('/background_process_test')
 def background_process_test():
    return("nothing")
+
+@app.route('/buttons_flex_row', methods=['GET'])
+def buttons_flex_row():
+   value = request.args.get('value')
+   response = 'Function was run successfully with value: ' + str(value)
+   return response
 
 def printError(e):
    print("There was an issue saving your profile information to the database... Please try again later. Error message:\n\n\n" + str(e))
@@ -181,5 +247,5 @@ def stringToList(string):
    return string[2:-2].split("', '")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   app.run(ssl_context=context, debug=True)
 
